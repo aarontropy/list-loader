78c2e1b8b41347bd62f79fff4ca8996b5aeeddba
Aaron Johnson <aarontropy@gmail.com>
2014-02-27 13:05:21 -0500

    Async user transform working

--- app.js
+++ app.js
@@ -1,74 +1,82 @@
 var app = angular.module('listloader.app', []);
 
 
-app.directive('listLoader', function() {
-	return {
-		restrict: "A",
-		template: "<div class=\"listLoader\">" +
-            "<div ng-repeat=\"line in lines track by $index\">{{line.text}}" +
-            "<span ng-hide=\"line.complete\" style=\"float: right\">loading</span></div>" +
+app.directive('listLoader', ['$timeout', function($timeout) {
+    return {
+        restrict: "A",
+        template: "<div class=\"listLoader\">" +
+            "<div ng-repeat=\"object in objects track by $index\">{{displayText(object)}}" +
+            "<span ng-hide=\"object.$complete\" style=\"float: right\">loading</span></div>" +
             "<div class=\"listInput\" contenteditable></div>" +
             "</div>",
-		scope: {
-			lineaction: '=action',
-		},
-		controller: function($scope, $q) { 
-            $scope.lines = []; 
-            $scope.doAction = function(line) {
-                var deferred = $q.defer();
-
-                setTimeout(function() {
-                    line.output = $scope.lineaction(line.input);
-                    deferred.resolve(line);
-                },0);
-
-                return deferred.promise;
-
+        scope: {
+            objects: "=list",
+            property: "@",
+            transform: "="
+        },
+        controller: function($scope) {
+            $scope.test = function() {
+                $scope.objects.push({text: "I was pushed"});
             }
         },
-		link: function(scope, element, attrs) {
-            var addLine = function(text) {
-                var line = {
-                    text: text,
-                    input: text, 
-                    output: '',
-                    complete: false,
-                };
-
-                scope.$apply(function() {
-                    // push a new line object on to lines array
-    				scope.lines.push(line);
-    			});
-                
-                scope.doAction(line).then(function(tline) {
-                    tline.text = tline.output;
-                    tline.complete = true;
-                });
+        link: function(scope, element, attrs) {
+            scope.displayText = function(object) {
+                if (object.$complete) {   
+                    return object.text;
+                } else {
+                    return object.$input;
+                }
             };
 
             var inputDiv = element[0].firstChild.lastChild;
             inputDiv.onkeydown = inputDiv.onkeypress = function(event) {
                 if (event.which === 13) {
                     var lines = event.srcElement.innerText.split('\n');
-                    angular.forEach(lines, function(line) { addLine(line); });
+                    angular.forEach(lines, function(line) { 
+                        // Angular will create a $watch on each object
+                        // since
+                        var obj = {
+                            $input: line,
+                            $complete: false,
+                        };
+
+                        scope.$apply(function() {
+                            scope.objects.push(obj);
+                        });
+
+                        scope.$evalAsync(function() {
+                            scope.transform(line,obj);
+                            obj.$complete = true;
+                        })
+
+                    });
+                    // scope.$apply();
+                    // scope.$apply();
+
+
                     // clear the contenteditable div for next entry
                     event.srcElement.innerText = '';
                     event.preventDefault();                 
-				}
-			}
+                }
+            }
 
-			// scope.$watch('lines', function(newVal, oldVal) { }, true);
-		}
-	}
-})
-
-app.controller('listloader.ctrl', function($scope) {
-	$scope.myAction = function(line) {
-        for (var i=0; i<1000000000; i++) {
-            k = i+1;
+            // scope.$watch('lines', function(newVal, oldVal) { }, true);
         }
+    }
+}])
+
+app.controller('listloader.ctrl', ['$scope', '$timeout', function($scope, $timeout) {
+    $scope.myObjects = [];
+
+    $scope.myTransform = function(inputText, object) {
+        $timeout(function() {
+            object.text = "I transformed " + inputText;
+        }, 1000);
+    }
+
+    $scope.myAction = function(line) {
        return "Converted input [" + line + "] to this output";
-	}
-})
+    }
+}])
 
 
